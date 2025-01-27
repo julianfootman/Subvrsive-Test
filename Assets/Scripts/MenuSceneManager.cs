4b23d1b180cf825d7de5b270552063814d820c29
Julian Footman <julianfootman1025@gmail.com>
2025-01-27 16:21:40 -0600

    set default value as 10

--- Assets/Scripts/MenuSceneManager.cs
+++ Assets/Scripts/MenuSceneManager.cs
@@ -6,6 +6,11 @@ public class MenuSceneManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField _playerCount;
 
+    private void Start()
+    {
+        _playerCount.text = "10";
+    }
+
     public void OnClickStartGame()
     {
         PlayerPrefs.SetInt("PlayerCount", int.Parse(_playerCount.text));
