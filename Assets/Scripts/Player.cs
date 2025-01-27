47f1342b853b221f268563bc7f47bc89f40f73aa
Julian Footman <julianfootman1025@gmail.com>
2025-01-27 16:21:40 -0600

    Added auto path finding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,36 @@ public class Player : MonoBehaviour
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private Transform _weaponPos;
 
+    [SerializeField] private State _currentState;
+    public enum State
+    {
+        Movement,
+        TargetingToEnemy
+    }
+
     public void Shot()
     {
         _animator.SetTrigger("Shot");
+    }
+
+    private void Update()
+    {
+        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            SetRandomDestination();
+        }
+
+        _animator.SetFloat("Move", _navMeshAgent.velocity.magnitude);
+    }
+
+    private void SetRandomDestination()
+    {
+        Vector3 randomPoint = transform.position + Random.insideUnitSphere * 10;
+        NavMeshHit hit;
+
+        if (NavMesh.SamplePosition(randomPoint, out hit, 30, NavMesh.AllAreas))
+        {
+            _navMeshAgent.SetDestination(hit.position);
+        }
     }
 }
